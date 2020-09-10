d48b68c952cf059f29c129bdd3fcd3b60f340734
Mayank-MP5 <mayank5pande@gmail.com>
2020-09-10 12:27:05 +0530

    Dummy Code to make router work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Link, Switch } from "react-router-dom";
+
 import Navbar from "./components/navbar";
+
+//Route Components Import
+import Home from "./components/home";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Dashboard from "./components/dashboard";
+
 function App() {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Link to="/" component={Home} />
+          <Link to="/dashboard" component={Dashboard} />
+          <Link to="/login" component={Login} />
+          <Link to="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </>
   );
 }

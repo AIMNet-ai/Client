98c2d04b85f1e33c8a7328a9c1cbcacc6178fe95
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-06 21:17:13 +0530

    Helpers Folder added

---
+++ src/helpers/auth.js
@@ -0,0 +1,8 @@
+//Each time system gets offline check it via user stored in localstorages
+export const checkAuth = () => {};
+
+export const login = () => {};
+
+export const signup = () => {};
+
+export const logout = () => {};

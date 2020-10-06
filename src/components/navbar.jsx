825a92c26ff3c162d72e52e69feb1739ebe924f2
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-06 21:15:02 +0530

    Links and Account Dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,9 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import { Menu, MenuItem } from "@material-ui/core";
+
 import aimnetIcon from "./../assets/ico-icon.ico";
 import { Link } from "react-router-dom";
 
@@ -23,7 +26,17 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Navbar() {
   const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div className={classes.root}>
       <AppBar position='static'>
@@ -39,11 +52,48 @@ export default function Navbar() {
             AIMNet
           </Typography>
           <Link to='/login'>
-            <Button color='primary'>Login</Button>
+            <Button
+              variant='outlined'
+              color='inherit'
+              style={{ color: "white", margin: "0 5px" }}>
+              Login
+            </Button>
           </Link>
           <Link to='/signup'>
-            <Button color='primary'>Signup</Button>
+            <Button
+              variant='outlined'
+              color='inherit'
+              style={{ color: "white", margin: "0 5px" }}>
+              Signup
+            </Button>
           </Link>
+          <div>
+            <IconButton
+              aria-label='account of current user'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleMenu}
+              color='inherit'>
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={open}
+              onClose={handleClose}>
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
